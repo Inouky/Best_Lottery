0b4a5ce9f9bdf54e4de4d208889e89fc4292e451
Inouky <francisc80@gmail.com>
2020-09-16 21:35:18 -0400

    Ventas Update 2

--- Best Lottery/Best Lottery/frmVentas.Designer.cs
+++ Best Lottery/Best Lottery/frmVentas.Designer.cs
@@ -32,8 +32,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
-            this.txtNumero = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -50,6 +48,8 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -66,7 +66,7 @@
             this.button1.Location = new System.Drawing.Point(406, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(222, 33);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 4;
             this.button1.Text = "Agregar jugada";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -91,33 +91,9 @@
             this.button2.Location = new System.Drawing.Point(406, 51);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(222, 33);
-            this.button2.TabIndex = 1;
+            this.button2.TabIndex = 5;
             this.button2.Text = "Finalizar jugada";
             this.button2.UseVisualStyleBackColor = false;
-            // 
-            // txtNumero
-            // 
-            this.txtNumero.BackColor = System.Drawing.Color.White;
-            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
-            this.txtNumero.ForeColor = System.Drawing.Color.DimGray;
-            this.txtNumero.Location = new System.Drawing.Point(79, 54);
-            this.txtNumero.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(275, 27);
-            this.txtNumero.TabIndex = 6;
-            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
-            this.textBox1.ForeColor = System.Drawing.Color.Transparent;
-            this.textBox1.Location = new System.Drawing.Point(79, 95);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 27);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // comboBox1
             // 
@@ -125,17 +101,17 @@
             this.comboBox1.Location = new System.Drawing.Point(79, 13);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(275, 24);
-            this.comboBox1.TabIndex = 10;
+            this.comboBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Enabled = false;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(406, 137);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(222, 23);
+            this.textBox2.Size = new System.Drawing.Size(222, 27);
             this.textBox2.TabIndex = 11;
             this.textBox2.Text = "$0";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -247,11 +223,11 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel3.Controls.Add(this.txtValor);
+            this.panel3.Controls.Add(this.txtNumero);
             this.panel3.Controls.Add(this.button3);
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Controls.Add(this.txtNumero);
             this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.textBox1);
             this.panel3.Controls.Add(this.label1);
             this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.textBox2);
@@ -283,9 +259,23 @@
             this.button3.Location = new System.Drawing.Point(406, 89);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(222, 33);
-            this.button3.TabIndex = 17;
+            this.button3.TabIndex = 6;
             this.button3.Text = "Imprimir ticket";
             this.button3.UseVisualStyleBackColor = false;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(79, 60);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(202, 22);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(79, 95);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(202, 22);
+            this.txtValor.TabIndex = 2;
             // 
             // frmVentas
             // 
@@ -315,8 +305,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox txtNumero;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Panel panel2;
@@ -334,5 +322,7 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtNumero;
     }
 }
